0036704205b51a352b0ac7afead467ad9f3dda29
Jan Kremer <jan.kremer@adform.com>
2017-06-19 16:09:09 +0200

    fixed weight-per-sample bug

--- vae.py
+++ vae.py
@@ -37,7 +37,7 @@ class VAE(object):
 
     def _create_weights(self, shape):
         W = tf.Variable(tf.random_normal(shape, stddev=self.stddev_init))
-        b = tf.Variable(np.zeros([shape[0], shape[1], 1]).astype(np.float32))
+        b = tf.Variable(np.zeros(shape[1]).astype(np.float32))
         return W, b
 
     def _create_weight_decay(self):
@@ -48,38 +48,36 @@ class VAE(object):
 
     def _create_encoder(self, x):
         with tf.variable_scope('encoder'):
-            W3, b3 = self._create_weights([self.n_batch, self.n_hidden, self.n_input])
-            W4, b4 = self._create_weights([self.n_batch, self.n_latent, self.n_hidden])
-            W5, b5 = self._create_weights([self.n_batch, self.n_latent, self.n_hidden])
+            W3, b3 = self._create_weights([self.n_input, self.n_hidden])
+            W4, b4 = self._create_weights([self.n_hidden, self.n_latent])
+            W5, b5 = self._create_weights([self.n_hidden, self.n_latent])
 
-            h = self.activation(W3 @ x + b3)
-            mu = W4 @ h + b4
-            log_sigma_squared = W5 @ h + b5
+            h = self.activation(x @ W3 + b3)
+            mu = h @ W4 + b4
+            log_sigma_squared = h @ W5 + b5
             sigma_squared = tf.exp(log_sigma_squared)
             sigma = tf.sqrt(sigma_squared)
         return mu, log_sigma_squared, sigma_squared, sigma
 
     def _create_decoder(self, z):
         with tf.variable_scope('decoder'):
-            W1, b1 = self._create_weights([self.n_batch, self.n_hidden, self.n_latent])
-            W2, b2 = self._create_weights([self.n_batch, self.n_input, self.n_hidden])
+            W1, b1 = self._create_weights([self.n_latent, self.n_hidden])
+            W2, b2 = self._create_weights([self.n_hidden, self.n_input])
 
-            y_logit = W2 @ self.activation(W1 @ z + b1) + b2
+            y_logit = self.activation(z @ W1 + b1) @ W2 + b2
             y = tf.sigmoid(y_logit)
         return y_logit, y
 
     def _create_model(self):
-        self.x = tf.placeholder(tf.float32, [self.n_batch, self.n_input])
-        x = tf.reshape(self.x, [self.n_batch, self.n_input, 1])
-        mu, log_sigma_squared, sigma_squared, sigma = self._create_encoder(x)
+        self.x = tf.placeholder(tf.float32, [None, self.n_input])
+        mu, log_sigma_squared, sigma_squared, sigma = self._create_encoder(self.x)
 
         self.epsilon = tf.placeholder(tf.float32, self.n_latent)
-        epsilon = tf.reshape(self.epsilon, [1, self.n_latent, 1])
-        self.z = mu + sigma * epsilon
+        self.z = mu + sigma * self.epsilon
         y_logit, self.y = self._create_decoder(self.z)
 
         regularizer = -0.5 * tf.reduce_sum(1 + log_sigma_squared - tf.square(mu) - sigma_squared, 1)
-        recon_error = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_logit, labels=x), 1)
+        recon_error = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_logit, labels=self.x), 1)
         weight_decay = self._create_weight_decay()
         loss = tf.reduce_mean(regularizer + recon_error) + weight_decay
         return loss
@@ -90,18 +88,18 @@ class VAE(object):
         optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=self.current_step)
         return optimizer
 
-    def fit(self, X, refit=False):
+    def fit(self, X, refit=True):
         graph = self._create_graph()
         session = tf.Session(graph=graph)
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(session, coord)
 
         try:
-            session.run(self.initializer) if not refit else self.saver.restore(session, self.checkpoint_path)
+            session.run(self.initializer) if refit else self.saver.restore(session, self.checkpoint_path)
             initial_step = session.run(self.current_step)
             start = time.time()
             for step in range(initial_step, self.n_step):
-                x = X.next_batch(self.n_batch)[0]
+                x, _ = X.next_batch(self.n_batch)
                 epsilon = np.random.randn(self.n_latent).astype(np.float32)
                 loss, _ = session.run([self.loss, self.optimizer], {self.x: x, self.epsilon: epsilon})
 
@@ -128,8 +126,7 @@ class VAE(object):
         with tf.Session(graph=graph) as session:
             self.saver.restore(session, self.checkpoint_path)
             n_iter = n_batch // self.n_batch if n_batch is not None else 1
-            batch = lambda i: z[i * self.n_batch:(i + 1) * self.n_batch, :, np.newaxis]
-            y = [session.run(self.y, {self.z: batch(i)}) for i in range(n_iter)]
+            y = [session.run(self.y, {self.z: z[i * self.n_batch:(i + 1) * self.n_batch]}) for i in range(n_iter)]
         return y
 
     def mosaic(self, grid_width=20):
